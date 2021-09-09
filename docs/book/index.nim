54187fe7b3f760ff7c218f36aa6163d169dc2ba5
Kerry Cobb <cobbkerry@gmail.com>
2021-09-08 22:46:32 -0500

    Change how readme is read

--- docs/book/index.nim
+++ docs/book/index.nim
@@ -1,13 +1,6 @@
 import nimib, nimibook
-import strutils
+
 nbInit
-nbUseNimibook # overrides nimib defaults with nimibooks and loads book configuration
-
-proc readFileUntil(filename: string, text: string): string =
-  for line in filename.lines:
-    if line.startsWith(text):
-      return result
-    result &= line & '\n'
-
-nbText: "../../README.md".readFileUntil("<!--SKIP")
+nbUseNimibook 
+nbText: readFile("../../README.md")
 nbSave
