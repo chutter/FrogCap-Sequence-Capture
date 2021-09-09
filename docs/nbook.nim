fdf45fd4c76aa63dae303d52b06fb32a425eec51
Kerry Cobb <cobbkerry@gmail.com>
2021-09-09 00:00:07 -0500

    fav icon

--- docs/nbook.nim
+++ docs/nbook.nim
@@ -13,5 +13,6 @@ var book = newBookFromToc("FrogCap", "book"):
 
 book.git_repository_url = "https://github.com/chutter/FrogCap-Sequence-Capture"
 book.preferred_dark_theme = "coal"
+book.favicon_escaped = """<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2280%22>&#f52e;</text></svg>">"""
 
 nimibookCli(book)
